72d832a220681f9a3a0dd51ee96d3b2f73f42580
vivek kumar <vivek.official004@gmail.com>
2025-06-14 13:20:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://job-app-mern-4wrg.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
