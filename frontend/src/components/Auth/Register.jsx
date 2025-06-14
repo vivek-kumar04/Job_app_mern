16eccd890431ae2a2f48b0c9ae81fc6d7580ce53
vivek kumar <vivek.official004@gmail.com>
2025-06-14 13:17:02 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://job-app-mern-4wrg.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
