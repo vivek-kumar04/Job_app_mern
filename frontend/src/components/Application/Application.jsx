da8796dd281ea3c1dfe0f77d1c254eb58f0b89f6
vivek kumar <vivek.official004@gmail.com>
2025-06-14 13:14:51 +0530

    Update Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -72,7 +72,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://job-app-mern-4wrg.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
